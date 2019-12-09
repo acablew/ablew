d0b540f3da9387319f970c09794e7349603fe19a
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-12-09 16:29:15 +0530

    Fixed site map

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -30,8 +30,29 @@ SitemapGenerator::Sitemap.create do
   # add '/home/contact'
   # add '/home/career'
   # add posts_path, :priority => 0.7, :changefreq => 'weekly'
+  add '/states'
+  add '/states/new'
+  add '/users'
+  add '/scholarview'
+  add '/singleposts'
+  add '/keywords'
+  add '/pages'
+
   Post.find_each do |post|
     add post_path(post), :lastmod => post.updated_at, :priority => 0.3, :changefreq => 'daily'
   end
+
+  Category.find_each do |category|
+    add category_path(category), :lastmod => category.updated_at, :priority => 0.3, :changefreq => 'daily'
+  end
+
+  Scholarship.find_each do |scholarship|
+    add scholarship_path(scholarship), :lastmod => scholarship.updated_at, :priority => 0.3, :changefreq => 'daily'
+  end
+
+  Page.find_each do |page|
+    add page_path(page), :lastmod => page.updated_at, :priority => 0.3, :changefreq => 'daily'
+  end
+
 end
 # SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
